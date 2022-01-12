4a446e5ed8b6e2380ee05b75f71116d4327a27ba
EttyDaniel <danieleti@gmail.com>
2022-01-12 19:17:34 +0000

    stores with 1M or more in revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "The total annual revenue of all the stores is : #{Store.sum(:annual_revenue)}"
+puts "The average annual revenue of all the stores is : #{Store.average(:annual_revenue)}"
+@store_big_revenue = Store.where("annual_revenue >= 1000000")
+puts "The number of stores that has 1M or more in revenue is: #{@store_big_revenue.count}"
